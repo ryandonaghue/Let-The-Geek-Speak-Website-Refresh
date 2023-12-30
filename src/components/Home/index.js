42f6eb8226c6315b15575ca10d616dafd9db09c2
ryandonaghue <ryandonaghue@outlook.com>
2023-12-30 14:45:07 +0000

    Added Letter Animation Functionality

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,19 +1,32 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom'
 import LogoTitle from '../../assets/images/logo-s.png';
+import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 
 
 const Home = () => {
-
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
+    const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
 
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br/> I'm
+                <h1>
+                <span className={letterClass}>H</span>
+                <span className={`${letterClass} _12`}>i</span>
+                <br/>
+                <span className={`${letterClass} _13`}>I</span>
+                <span className={`${letterClass} _14`}>'m</span>
                 <img src={LogoTitle} alt="developer" />
-                lobodan
+                <AnimatedLetters letterClass={letterClass}
+                strArray={nameArray}
+                idx={15} />
                 <br />
-                web developer
+                <AnimatedLetters letterClass={letterClass}
+                strArray={jobArray}
+                idx={22} />
                 </h1>
                 <h2>Frontend Developer / JavaScript Expert / Youtuber</h2>
                 <Link to="/contact" className='flat-button'>CONTACT</Link>
