11c5d0af68349e2b7272383a1b3b911a27715b08
ryandonaghue <ryandonaghue@outlook.com>
2023-12-31 09:25:57 +0000

    Fixed Animated Letters Bug

--- src/components/AnimatedLetters/index.js
+++ src/components/AnimatedLetters/index.js
@@ -5,7 +5,7 @@ const AnimatedLetters = ({ letterClass, strArray, idx }) => {
         <span>
             {
                 strArray.map((char, i)  =>(
-                    <span key={char + i} className='`${letterClass} _${i + idx}'>
+                    <span key={char + i} className={`${letterClass} _${i + idx}`}>
                         {char}
                     </span>
                 ))
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -8,14 +8,14 @@ import './index.scss';
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
     const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
-    const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
+    const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.']
 
     return (
         <div className="container home-page">
             <div className="text-zone">
                 <h1>
                 <span className={letterClass}>H</span>
-                <span className={`${letterClass} _12`}>i</span>
+                <span className={`${letterClass} _12`}>i,</span>
                 <br/>
                 <span className={`${letterClass} _13`}>I</span>
                 <span className={`${letterClass} _14`}>'m</span>
