706b2470fe04c9ed41d9c68100879ad3ccfe1e94
ryandonaghue <ryandonaghue@outlook.com>
2024-01-02 19:56:36 +0000

    Fixed Uncaught runtime errors.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'
 import LogoTitle from '../../assets/images/logo-s.png';
 import AnimatedLetters from '../AnimatedLetters';
@@ -8,7 +8,37 @@ import './index.scss';
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
     const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
-    const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.']
+    const jobArray = [
+        'w', 
+        'e', 
+        'b', 
+        ' ', 
+        'd', 
+        'e', 
+        'v', 
+        'e', 
+        'l', 
+        'o', 
+        'p', 
+        'e', 
+        'r', 
+        '.',]
+
+    useEffect(() => {
+        setTimeout(() => {
+            return setLetterClass('text-animate-hover')
+        }, 4000)
+    }, [])
+
+    // useEffect(() => {
+    //     const timeoutId = setTimeout(() => {
+    //       setLetterClass('text-animate-hover');
+    //     }, 4000);
+      
+    //     // Return a cleanup function
+    //     return () => clearTimeout(timeoutId);
+    //   }, []);
+
 
     return (
         <div className="container home-page">
@@ -29,8 +59,9 @@ const Home = () => {
                 idx={22} />
                 </h1>
                 <h2>Frontend Developer / JavaScript Expert / Youtuber</h2>
-                <Link to="/contact" className='flat-button'>CONTACT</Link>
+                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
+
         </div>
     )
 
