b17eeb276a9f88e93c0a7c0464a6c67a77d52eb6
ryandonaghue <ryandonaghue@outlook.com>
2024-01-03 15:08:33 +0000

    Added Logo

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'
 import LogoTitle from '../../assets/images/logo-s.png';
 import AnimatedLetters from '../AnimatedLetters';
+import Logo from './Logo';
 import './index.scss';
 
 
@@ -61,7 +62,7 @@ const Home = () => {
                 <h2>Frontend Developer / JavaScript Expert / Youtuber</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
-
+            <Logo />
         </div>
     )
 
